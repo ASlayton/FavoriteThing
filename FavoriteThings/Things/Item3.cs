433258c728034d27a2beaf6ec191dc452c8f3699
Amanda Slayton <alslayton86@gmail.com>
2018-08-29 20:38:15 -0500

    setup class files

---
+++ FavoriteThings/Things/Item3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings.Things
+{
+    class Item3 : ThingBase
+    {
+        //Constructor for Item 3- pass in property of first to the item base
+        public Item3() : base("third")
+        { }
+
+        //Print the name of the item in the console
+        public override void PrintItem()
+        {
+            Console.WriteLine($"Your third favorite tv show is {Item}");
+        }
+    }
+}
---
+++ FavoriteThings/Things/Item4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings.Things
+{
+    class Item4 : ThingBase
+    {
+        //Constructor for Item 4- pass in property of first to the item base
+        public Item4() : base("fourth")
+        { }
+
+        //Print the name of the item in the console
+        public override void PrintItem()
+        {
+            Console.WriteLine($"Your fourth favorite tv show is {Item}");
+        }
+    }
+}
